a41c12826a36f1c5ca2cc384ff120d35cec57557
Rohit Yadav <yadavrohit7212@gmail.com>
2026-06-28 01:29:34 +0530

    New content added

---
+++ store_index.py
@@ -0,0 +1,44 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+from src.helper import load_pdf_files, filter_to_minimal_docs, text_split, download_embeddings
+from pinecone import Pinecone
+from pinecone.models import ServerlessSpec
+
+from langchain_pinecone import PineconeVectorStore
+
+
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY 
+
+
+extracted_data = load_pdf_files("Data")
+minimal_docs= filter_to_minimal_docs(extracted_data)
+texts_chunks = text_split(minimal_docs)
+
+embedding = download_embeddings()
+
+pinecone_api_key = PINECONE_API_KEY
+pc = Pinecone(api_key=pinecone_api_key)
+
+
+index_name = "medimind"
+
+if not pc.has_index(index_name):
+    pc.create_index(
+        name=index_name,
+        dimension=384,
+        metric="cosine",
+        spec=ServerlessSpec(cloud="aws", region="us-east-1")
+    )
+index = pc.Index(index_name)
+
+
+docsearch = PineconeVectorStore.from_documents(
+    documents=texts_chunks,
+    embedding=embedding,
+    index_name=index_name
+)

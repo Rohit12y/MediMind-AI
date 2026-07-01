dbdda1f799ee8b7b2cb50e98bf3c2fa248d65302
Rohit Yadav <yadavrohit7212@gmail.com>
2026-07-02 00:02:33 +0530

    WEN APP COMPLETED

--- store_index.py
+++ store_index.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 import os
 load_dotenv()
-from src.helper import load_pdf_files, filter_to_minimal_docs, text_split, download_embeddings
+from src.helper import load_pdf_files, filter_to_minimal_docs, text_split, download_embeddings, download_HuggingFaceEmbeddings
 from pinecone import Pinecone
 from pinecone.models import ServerlessSpec
 
@@ -19,7 +19,7 @@ extracted_data = load_pdf_files("Data")
 minimal_docs= filter_to_minimal_docs(extracted_data)
 texts_chunks = text_split(minimal_docs)
 
-embedding = download_embeddings()
+embedding =download_HuggingFaceEmbeddings()
 
 pinecone_api_key = PINECONE_API_KEY
 pc = Pinecone(api_key=pinecone_api_key)
@@ -41,4 +41,4 @@ docsearch = PineconeVectorStore.from_documents(
     documents=texts_chunks,
     embedding=embedding,
     index_name=index_name
-)
+) 
